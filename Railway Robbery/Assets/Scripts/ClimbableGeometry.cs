67f0dc31dea8d03205e3ac038ca50a08a8b480d8
grantkeele <31322729+grantkeele@users.noreply.github.com>
2020-08-08 14:06:11 -0400

    Added bare minimum climbing script to pull body while grip is held

---
+++ Railway Robbery/Assets/Scripts/ClimbableGeometry.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbableGeometry : MonoBehaviour
+{
+
+    public Mesh mesh;
+    public PhysicMaterial physicMaterial;
+
+
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Railway Robbery/Assets/Scripts/ClimbingManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbingManager : MonoBehaviour
+{
+
+    [SerializeField] private Transform leftController;
+    [SerializeField] private Transform rightController;
+
+    [SerializeField] private float climbingGripThreshold = 0.5f;
+
+    private bool leftGripHeld = false;
+    private bool rightGripHeld = false;
+    private Vector3 leftGripAnchor;
+    private Vector3 rightGripAnchor;
+    private Vector3 bodyAnchor;
+    private Vector3 targetBodyPosition;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        OVRInput.FixedUpdate();
+
+        // Set anchor point on the first frame grip is held
+        if (!leftGripHeld){ 
+            leftGripAnchor = leftController.transform.position;
+            bodyAnchor = this.transform.position;
+        }
+        if (!rightGripHeld){ 
+            rightGripAnchor = rightController.transform.position;
+            bodyAnchor = this.transform.position;
+        }
+        
+        // Read grip input for each hand
+        leftGripHeld = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch) >= climbingGripThreshold ? true : false;
+        rightGripHeld = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.RTouch) >= climbingGripThreshold ? true : false;
+        //Debug.Log(leftGripHeld);
+
+        // Calculate hand position relative to the anchor point and move the player body accordingly
+        if (leftGripHeld){
+            targetBodyPosition = leftGripAnchor - leftController.transform.position;
+            this.transform.position = bodyAnchor + targetBodyPosition;
+        }
+        else if (rightGripHeld){
+            targetBodyPosition = rightGripAnchor - rightController.transform.position;
+            this.transform.position = bodyAnchor + targetBodyPosition;
+        }
+    }
+}
