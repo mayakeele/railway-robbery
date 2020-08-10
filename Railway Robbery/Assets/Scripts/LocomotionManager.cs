286ab9386f1c7cd5a9907d702707bf4b4eb9f7d8
grantkeele <31322729+grantkeele@users.noreply.github.com>
2020-08-10 00:03:33 -0400

    Smooth locomotion with input affecting velocity

--- Railway Robbery/Assets/Scripts/LocomotionManager.cs
+++ Railway Robbery/Assets/Scripts/LocomotionManager.cs
@@ -4,25 +4,64 @@ using UnityEngine;
 
 public class LocomotionManager : MonoBehaviour
 {
+    
+    [SerializeField] private bool useHeadAsForward;
+    private Quaternion targetOrientation;
+
+    [SerializeField] private float maxMovementSpeed;
+    [SerializeField] private float linearAcceleration;
+
+    [SerializeField] private float maxRotationSpeed;
+    [SerializeField] private float angularAcceleration;
+
+    [SerializeField] private float stickDeadzone;
+
+    private Vector3 linearVelocity = Vector3.zero;
+    private float angularVelocity = 0;
+
     private InputHandler inputHandler;
-
-    [SerializeField] private float movementSpeed;
-    [SerializeField] private float rotationSpeed;
-
+    private Rigidbody rigidbody;
+    
     void Start()
     {
         inputHandler = GetComponent<InputHandler>();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
 
     void FixedUpdate()
-    {
-        // Handle player movement with the left joystick
-        Vector2 movementInput = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick) * movementSpeed * Time.deltaTime;
-        Vector3 movementVector = inputHandler.leftController.rotation * new Vector3(movementInput.x, 0, movementInput.y);
-        this.transform.position += movementVector;
+    {   
+        // Get left stick input and determine whether 'forward' should be based on controller orientation or head orientation
+        targetOrientation = useHeadAsForward ? inputHandler.cameraTransform.rotation : inputHandler.leftController.rotation;
+        Vector2 movementInput = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, OVRInput.Controller.LTouch);
+        
+        // Combine joystick input and forward direction to determine the direction of acceleration
+        Vector3 movementDirection = targetOrientation * new Vector3(movementInput.x, 0, movementInput.y).normalized;
 
-        float rotationInput = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x * rotationSpeed * Time.deltaTime;
+        // Apply acceleration if the stick is pushed outside the deadzone, clamping velocity with regard to max speed AND the magnitude of the input
+        /*if (movementInput.magnitude >= stickDeadzone) {    
+            linearVelocity += movementDirection * linearAcceleration * Time.deltaTime;
+            linearVelocity = Vector3.ClampMagnitude(linearVelocity, maxMovementSpeed * movementInput.magnitude);
+        }
+        // If stick is not pushed past deadzone, decelerate the player to zero
+        else{
+            movementInput = Vector2.zero;
+            if (linearVelocity.magnitude > 0){
+                linearVelocity -= linearVelocity.normalized * linearAcceleration * Time.deltaTime;
+            }
+            else{
+                linearVelocity = Vector2.zero;
+            }
+        }*/
+
+        linearVelocity += movementDirection * linearAcceleration * Time.deltaTime;
+        linearVelocity = Vector3.ClampMagnitude(linearVelocity, maxMovementSpeed * movementInput.magnitude);
+
+        // Set x and z components of movement while preserving y
+        this.rigidbody.velocity = new Vector3(linearVelocity.x, this.rigidbody.velocity.y, linearVelocity.z);
+
+
+        float rotationInput = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x * maxRotationSpeed * Time.deltaTime;
         this.transform.RotateAround(inputHandler.cameraTransform.position, Vector3.up, rotationInput);
     }
 }
