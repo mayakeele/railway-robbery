495e76f3328e21830a1449f60063d58e6e50bad9
grantkeele <31322729+grantkeele@users.noreply.github.com>
2020-08-11 23:59:35 -0400

    Updated all player-associated rigidbodies to Interpolate and Continuous collision detection
    Also standardized hand radius references across multiple gameobjects to give control to one variable. Also changed physical hand force to not scale with distance.

--- Railway Robbery/Assets/Scripts/ClimbingManager.cs
+++ Railway Robbery/Assets/Scripts/ClimbingManager.cs
@@ -10,6 +10,7 @@ public class ClimbingManager : MonoBehaviour
 
     [SerializeField] private float handRadius = 0.08f;
     [SerializeField] private int armForce = 900; // (in newtons)
+    [SerializeField] private int handForce = 20; // (in newtons)
     [SerializeField] private int bodyWeight = 625; // (in newtons)
 
 
@@ -33,6 +34,12 @@ public class ClimbingManager : MonoBehaviour
     void Start()
     {
         inputHandler = GetComponent<InputHandler>();
+
+        inputHandler.leftControllerAnchor.GetComponent<SphereCollider>().radius = handRadius;
+        inputHandler.rightControllerAnchor.GetComponent<SphereCollider>().radius = handRadius;
+
+        inputHandler.leftPhysicsHand.localScale = new Vector3(handRadius * 2, handRadius * 2, handRadius * 2);
+        inputHandler.rightPhysicsHand.localScale = new Vector3(handRadius * 2, handRadius * 2, handRadius * 2);
     }
 
     void FixedUpdate()
@@ -47,19 +54,21 @@ public class ClimbingManager : MonoBehaviour
         // Attempt to match physics hand position with player's real life hands.
         if (leftHandColliding == true){
             Vector3 leftPhysicsHandOffset = inputHandler.leftControllerAnchor.position - inputHandler.leftPhysicsHand.position;
-            inputHandler.leftPhysicsHand.GetComponent<Rigidbody>().AddForce(leftPhysicsHandOffset * armForce, ForceMode.Force);
+            inputHandler.leftPhysicsHand.GetComponent<Rigidbody>().AddForce(leftPhysicsHandOffset.normalized * handForce, ForceMode.Force);
         }
         else{
             inputHandler.leftPhysicsHand.position = inputHandler.leftControllerAnchor.position;
+            inputHandler.leftPhysicsHand.GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
         inputHandler.leftPhysicsHand.rotation = inputHandler.leftControllerAnchor.rotation;
 
         if (rightHandColliding == true){
             Vector3 rightPhysicsHandOffset = inputHandler.rightControllerAnchor.position - inputHandler.rightPhysicsHand.position;
-            inputHandler.rightPhysicsHand.GetComponent<Rigidbody>().AddForce(rightPhysicsHandOffset * armForce, ForceMode.Force);
+            inputHandler.rightPhysicsHand.GetComponent<Rigidbody>().AddForce(rightPhysicsHandOffset.normalized * handForce, ForceMode.Force);
         }
         else{
             inputHandler.rightPhysicsHand.position = inputHandler.rightControllerAnchor.position;
+            inputHandler.rightPhysicsHand.GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
         inputHandler.rightPhysicsHand.rotation = inputHandler.rightControllerAnchor.rotation;
 
