f285e9332711c6b29d6eda977326d02d79e6cb27
grantkeele <31322729+grantkeele@users.noreply.github.com>
2020-08-15 20:38:11 -0400

    Added damped oscillation script

---
+++ Railway Robbery/Assets/Scripts/DampedOscillation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DampedOscillation
+{
+    public static Vector3 GetDampedSpringForce(Transform objectTransform, Transform targetTransform, float springConstant, float dampingRatio){
+        // Calculates the combined force of a spring and damping force on an object
+
+        Rigidbody rb = objectTransform.GetComponent<Rigidbody>();
+
+        Vector3 velocity = rb.velocity;
+        Vector3 dispacement = objectTransform.position - targetTransform.position;
+        float mass = rb.mass;
+
+        Vector3 force = (-2 * dampingRatio * velocity * Mathf.Sqrt(mass * springConstant)) - (springConstant * dispacement);
+
+        return force;
+    }
+}
