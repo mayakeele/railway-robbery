dabf1f987d8a32c2852e9e8134a25b73d9a77e89
Grant Keele <31322729+grantkeele@users.noreply.github.com>
2020-08-16 01:17:14 -0400

    Added Damped Oscillation script and function, created simple testing scene for demonstration

--- Railway Robbery/Assets/Scripts/DampedOscillation.cs
+++ Railway Robbery/Assets/Scripts/DampedOscillation.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public static class DampedOscillation
 {
-    public static Vector3 GetDampedSpringForce(Transform objectTransform, Transform targetTransform, float springConstant, float dampingRatio){
+    public static Vector3 GetDampedSpringForce(Transform objectTransform, Transform targetTransform, float mass, float springConstant, float dampingRatio){
         // Calculates the combined force of a spring and damping force on an object
 
         Rigidbody rb = objectTransform.GetComponent<Rigidbody>();
 
         Vector3 velocity = rb.velocity;
         Vector3 dispacement = objectTransform.position - targetTransform.position;
-        float mass = rb.mass;
 
         Vector3 force = (-2 * dampingRatio * velocity * Mathf.Sqrt(mass * springConstant)) - (springConstant * dispacement);
 
---
+++ Railway Robbery/Assets/Scripts/TEST_cube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_cube : MonoBehaviour
+{   
+    public float springConstant;
+    public float dampingRatio;
+    public Transform target;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 force = DampedOscillation.GetDampedSpringForce(this.transform, target.transform, rb.mass, springConstant, dampingRatio);
+        rb.AddForce(force);
+    }
+}
