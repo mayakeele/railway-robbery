0c05b610c99bd358ad4f4f15a6f8dcdd49f4f932
Grant Keele <31322729+grantkeele@users.noreply.github.com>
2020-09-03 20:07:10 -0400

    Added hinge joint to caboose door, created HingedDoor.cs

---
+++ Railway Robbery/Assets/Scripts/Train/Parts/HingedDoor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HingedDoor : MonoBehaviour
+{
+    Vector3 hingeAxis;
+    Vector3 hingeAnchor;
+
+    private Rigidbody frameRigidbody;
+    private Rigidbody doorRigidbody;
+    private BoxCollider boxCollider;
+
+    [SerializeField] private HingeJoint hinge;
+
+
+    void Start()
+    {
+        frameRigidbody = GetComponentInParent<Rigidbody>();
+        doorRigidbody = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
