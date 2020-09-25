89b50774cf4247922ce9b6aab14d1d10f890b3c4
Grant Keele <31322729+grantkeele@users.noreply.github.com>
2020-09-25 18:06:32 -0400

    Added Theme Layers, which are used to represent a group of train parts with the same color and material
    Each theme layer has a finite number of colors in its palette, and each train car randomly chooses one of these colors for each layer and then assigns those colors to its parts

---
+++ Railway Robbery/Assets/Scripts/Train/Themes/ThemeLayers.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ThemeLayers
+{
+    public enum ThemeLayer{
+        Wall_Primary,
+        Wall_Detail,
+        Roof_Primary,
+        Roof_Detail,
+        Floor_Primary,
+        Glass_Primary,
+        Metal_Primary_Exterior,
+        Metal_Primary_Interior,
+    }
+
+    public static int GetLayerCount(){
+        int numLayers = System.Enum.GetNames(typeof(ThemeLayer)).Length;
+        return numLayers;
+    }
+}
