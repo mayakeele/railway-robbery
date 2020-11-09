bb748a9d4537df7a47e99f79ea756eea66202835
Grant Keele <31322729+grantkeele@users.noreply.github.com>
2020-11-08 19:51:12 -0500

    Added legs and leg manager script to test enemy

---
+++ Railway Robbery/Assets/Scripts/NPC/LegManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DitzelGames.FastIK;
+
+public class LegManager : MonoBehaviour
+{
+    public float displacementToChangeTarget;
+    public List<FastIKFabric> legs;
+
+    public List<Transform> defaultTargets;
+
+    public List<Transform> currentTargets;
+    public List<Transform> potentialTargets;
+
+    void Start()
+    {
+        for(int i = 0; i < legs.Count; i++){
+            FastIKFabric currentLeg = legs[i];
+
+            currentTargets.Add(Instantiate(defaultTargets[i]));
+
+            currentLeg.Target = currentTargets[i];
+        }
+    }
+
+    void Update()
+    {
+        for(int i = 0; i < legs.Count; i++){
+            FastIKFabric currentLeg = legs[i];
+            Transform currentTarget = currentTargets[i];
+            Transform defaultTarget = defaultTargets[i];
+
+            float displacementFromDefault = (currentTarget.position - defaultTarget.position).magnitude;
+            if(displacementFromDefault >= displacementToChangeTarget){
+                currentTarget.position = defaultTarget.position;
+                currentLeg.Target = currentTarget;
+            }
+        }
+    }
+}
